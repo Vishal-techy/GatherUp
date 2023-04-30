1b050b2ae7de6bb3c001369b61a18ab35ce6fd0b
Sasidharan <dharan731@gmail.com>
2023-04-30 14:09:10 +0530

    socials

--- components/promotionCard.js
+++ components/promotionCard.js
@@ -3,26 +3,50 @@ import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import { useState } from 'react';
 import axios  from 'axios';
+import Image from 'next/image'
+
 function PromotionCard({eventId}) {
     const [loading, setLoading] = useState(false)
+    const [poster, setPoster] = useState()
+    const [uploading, setUploading] = useState(false)
     const [data, setData] = useState({
-        title: '',
-        description: '',})
+        eventName : '',
+        eventSubText : '',
+        link : '',
+        aboutCompany : '',
+        name : '',
+        date : '',
+
+    })
 
     const postContent = (e) => {
       e.preventDefault()
-      console.log("Hey");
+      console.log(data);
       setLoading(true)
-      axios.post(`http://localhost:8080/sendMessage/${eventId}`, {
-        title: data.title,
-        description: data.description,
-      }).then((res) => {
-
+      axios.post(`http://localhost:8080/createImage/`, data).then((res) => {
+        console.log(res.data);
+        setPoster(res.data.imageUrl)
         setLoading(false)
       }).catch((err) => {
          setLoading(false)
          alert("Something went wrong")
-      })  
+      }) 
+    
+    }
+
+    const postImageInsta = () => {
+        
+        setUploading(true)
+        axios.post(`http://localhost:8080/createImage/postInsta`, {
+            url : poster.image_url_jpg
+        }).then((res) => {
+            console.log(res.data);
+            setUploading(false)
+            alert("Posted Successfully")
+        }).catch((err) => {
+             setUploading(false)
+             alert("Something went wrong")
+        })
     }
   return (
     <>
@@ -30,69 +54,70 @@ function PromotionCard({eventId}) {
     <div className='NotificationCentercardWrap'>    
     <h2 className='NotificationCard-hero'>Social media post</h2>
     <div>
-    <Form>
+    <Form onSubmit={postContent}>
       <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Control onChange={(e) =>
               setData({
                 ...data,
-                title: e.target.value,
+                eventName: e.target.value,
               })
             }
-            value={data.title} className='emailInput2' placeholder='Title'/>
+            value={data.eventName} className='emailInput2' placeholder='Event Name'/>
       </Form.Group>
 
       <Form.Group className="mb-3" controlId="formBasicPassword">
         <Form.Control onChange={(e) =>
               setData({
                 ...data,
-                description: e.target.value,
+                eventSubText : e.target.value,
               })
             }
-            value={data.description}
-            required type="text" className='emailInput2' placeholder="Description" />
+            value={data.eventSubText}
+            required type="text" className='emailInput2' placeholder="Event Sub Text" />
       </Form.Group>
       <div className='flexFor-input'>
       <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Control onChange={(e) =>
               setData({
                 ...data,
-                title: e.target.value,
+              link : e.target.value,
               })
             }
-            value={data.title} className='input3' placeholder='Title'/>
+            value={data.link} className='input3' placeholder='Website link or Video link'/>
       </Form.Group>
 
       <Form.Group className="mb-3" controlId="formBasicPassword">
         <Form.Control onChange={(e) =>
               setData({
                 ...data,
-                description: e.target.value,
+                name : e.target.value,
               })
             }
-            value={data.description}
-            required type="text" className='input3' placeholder="Description" />
+            value={data.name}
+            required type="text" className='input3' placeholder="Name" />
       </Form.Group>
+      
       </div>
       <div className='flexFor-input'>
       <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Control onChange={(e) =>
               setData({
                 ...data,
-                title: e.target.value,
+                date: e.target.value,
               })
             }
-            value={data.title} className='input3' placeholder='Title'/>
+            value={data.title} className='input3' placeholder='Date'/>
       </Form.Group>
 
       <Form.Group className="mb-3" controlId="formBasicPassword">
         <Form.Control onChange={(e) =>
               setData({
                 ...data,
-                description: e.target.value,
+                aboutCompany: e.target.value,
               })
             }
-            value={data.description}
-            required type="text" className='input3' placeholder="Description" />
+            value={data.aboutCompany}
+            required type="text" className='input3' placeholder="About Company" />
       </Form.Group>
       </div>
       <div className='sub-btn-wrap'>
@@ -105,12 +130,31 @@ function PromotionCard({eventId}) {
     </div>
     </div>
     <div className='poster'>
+        {
+            // loading ?
+            // !poster ?
+            // <div>Preparing image..</div> : 
+            // <Image src={poster.image_url_jpg} className='poster-img' width={400} height={628}/>
+            // : null
 
+            loading ?
+            <div>Preparing image..</div> :
+            poster ?
+            <Image src={poster.image_url_jpg} className='poster-img' width={400} height={628}/>
+            : null
+
+        }
     </div>
     </div>
+    {
+        poster ?
         <div className='posting'> 
-        <button className='submit-btn3'style={{color: 'white'}}>Post In Social Media</button>
-        </div>
+        {
+            !uploading ?
+            <button className='submit-btn3'style={{color: 'white'}} onClick={postImageInsta}>Post In Social Media</button> : <div>Posting...</div>
+        }
+        </div> : null
+    }
         </>
   )
 }
--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['images.bannerbear.com'],
+  },
 }
 
 module.exports = nextConfig
