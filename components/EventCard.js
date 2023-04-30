40b09e053c171ce7d5b9efb58bb25d2aa1e62924
Vishal-Techy <vishalchinns1650@gmail.com>
2023-04-30 08:08:14 +0530

    event card in progress

--- components/EventCard.js
+++ components/EventCard.js
@@ -1,4 +1,8 @@
+import Image from 'next/image'
 import React from 'react'
+import poster from '../public/eventPoster.png'
+import date from '../public/date.png'
+import mode from '../public/mode.png'
 
 function EventCard() {
   return (
@@ -17,22 +21,29 @@ function EventCard() {
                     </div>
             </div>
             
+            <hr style={{width:'85vw', marginTop:'15px'}}/>
 
             <div className='eventContent'>
                 <div className='eventPoster'>
-                    
+                    {/* <Image width={'10%'} src={poster}/> */}
                 </div>
                 <div className='eventDescription'>
                     <div className='eveDesc'>
                         Lorem simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
                     </div>
                     <div className='eveDateMode'>
-                        <div></div>
-                        <div></div>
+                        <div className='eveDM'>
+                            <Image style={{width:'2vw', height:'auto', marginRight:'5px'}} src={date}/>
+                            <p>April 30 to 31, 2023</p>
+                        </div>
+                        <div className='eveDM'>
+                            <Image style={{width:'2vw', height:'auto', marginRight:'5px'}} src={mode}/>
+                            <p>Online Event</p>
+                        </div>
                     </div>
                 </div>
-                <div className='preview'></div>
             </div>
+                <div className='preview'></div>
         </div>
     </div>
   )
